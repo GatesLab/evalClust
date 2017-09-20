46bd6d40599af253edc6ad0c453600142ed1cfa2
kgates <gateskm@email.unc.edu>
2017-09-20 09:19:21 -0400

    Fix abort issue; add corr option

--- R/evalClust.R
+++ R/evalClust.R
@@ -8,67 +8,75 @@
 library(mcclust) # move this to somewhere else?
 library(dils)
 library(igraph)
-library(ggplot2)
 
-evalClust <- function(sym.matrix){
-
+evalClust <- function(sym.matrix, type = "count"){
+  
   if (is.igraph(sym.matrix))
-  { g <- sym.matrix
+  { g <- sym.matrix # only recommended for count graphs; for correlation or those with (-) read in matrix
   sym.matrix <- as.sym.matrix( get.adjacency(g, attr = "weight"))
-  } else
+  } else{
+    sym.matrix <- as.data.frame(sym.matrix)
+    diag(sym.matrix)        <- 0
+    if (max(sym.matrix) < 1 & max(sym.matrix)>0)
+      sym.matrix <- round(sym.matrix, 3)
+    sym.matrix[sym.matrix < 0] <- 0
     g                   <- graph.adjacency(as.matrix(sym.matrix), mode = "undirected", weighted = TRUE)
-
-  diag(sym.matrix)        <- 0
-
+  }
+  
   truemembership          <- walktrap.community(g, weights = E(g)$weight, steps = 4)$membership
-
+  
   # now randomly perturb and rewire
+  reps <- 100
+  
   n.elements <- length(sym.matrix[,1])*(length(sym.matrix[,1])-1)/2
-  percent <-seq(from=0, to = n.elements, by=max(round(0.01*(n.elements)), 1)) #disrupt 1% at a time
-  VI<-matrix(,nrow = 100, ncol = length(percent))
-  ARI<-matrix(,nrow = 100,ncol = length(percent))
-  modularity.value <-matrix(,nrow = 100, ncol = length(percent))
-
+  percent <-seq(from=0, to = n.elements, by=max(round(0.02*(n.elements)), 1)) #disrupt 2% at a time
+  VI<-matrix(,nrow = reps, ncol = length(percent))
+  ARI<-matrix(,nrow = reps,ncol = length(percent))
+  modularity.value <-matrix(,nrow = reps, ncol = length(percent))
+  
   VI[,1] <- 0 #when 0 edges are disrupted no variation of information
   ARI[,1]<- 1 #when 0 edges are disrupted ARI = 1
-
-  for(k in 1:length(percent)) {
-    for(p in 1:100){ # for each degree of perturbation run 100 times
-      new.v <- as.matrix(rewirematrix(sym.matrix, percent[k]))
-      new.g                  <- graph.adjacency(new.v, mode = "undirected", weighted = TRUE)
-      membership       <- walktrap.community(new.g, steps = 4)$membership
-      modularity.value[p,k] <- modularity(walktrap.community(new.g, steps = 4))
-      ## ARI and VI for comparison of this.g and membership in original
+  
+  for(k in 2:length(percent)) {
+    for(p in 1:reps){ # for each degree of perturbation run 100 times
+      new.v <- as.matrix(rewirematrix(sym.matrix, percent[k], type))
+      new.g                  <- graph.adjacency(as.matrix(new.v), mode = "undirected", weighted = TRUE)
+      clust.sol       <-  walktrap.community(new.g, weights = E(new.g)$weight)
+      #saveRDS(new.v, "~/Dropbox/Perturb/Aidan Data/eval output/stoppedwalk.RDS")
+      membership       <- clust.sol$membership
+      modularity.value[p,k] <- modularity(clust.sol)
+      # ARI and VI for comparison of this.g and membership in original
       VI[p,k] <- vi.dist(membership, truemembership)
-      ARI[p,k] <-max(arandi(membership, truemembership), 0) #since ARI can be negative
+      ARI[p,k] <-arandi(membership, truemembership) #since ARI can be negative
     }
   }
-
+  
   # create random graph, rewire, compare to the above in a figure.
-  rando <- rewirematrix(sym.matrix, percent[length(percent)]) #create randomized network with same overall weight
-
-  VI.rando<-matrix(,nrow = 100, ncol = length(percent))
-  ARI.rando<-matrix(,nrow = 100,ncol = length(percent))
-  modularity.rando <-matrix(,nrow = 100, ncol = length(percent))
-
+  rando <- new.v
+  VI.rando<-matrix(,nrow = reps, ncol = length(percent))
+  ARI.rando<-matrix(,nrow = reps,ncol = length(percent))
+  modularity.rando <-matrix(,nrow = reps, ncol = length(percent))
+  
   VI.rando[,1] <- 0 #when 0 edges are disrupted no variation of information
   ARI.rando[,1]<- 1 #when 0 edges are disrupted ARI = 1
-
+  
   for(k in 2:length(percent)) {
-    for(p in 1:100){ # for each degree of perturbation run 100 times
-      new.v <-  as.matrix(rewirematrix(rando, percent[k]))
+    for(p in 1:reps){ # for each degree of perturbation run 100 times
+      new.v <-  as.matrix(rewirematrix(rando, percent[k], type))
       new.g                  <- graph.adjacency(new.v, mode = "undirected", weighted = TRUE)
-      membership       <- walktrap.community(new.g, steps = 4)$membership
-      modularity.rando[p,k] <- modularity(walktrap.community(new.g, steps = 4))
+      clust.sol       <-  walktrap.community(new.g, weights = E(new.g)$weight, steps = 4)
+      #saveRDS(new.v, "~/Dropbox/Perturb/Aidan Data/eval output/stoppedwalk.RDS")
+      membership       <- clust.sol$membership
+      modularity.rando[p,k] <- modularity(clust.sol)
       ## ARI and VI for comparison of this.g and membership in original
       VI.rando[p,k] <- vi.dist(membership, truemembership)
       ARI.rando[p,k] <-max(arandi(membership, truemembership), 0) #since ARI can be negative
     }
   }
-
+  
   # plots of ARI and VI compared to original
   percentlab <- percent/n.elements
-
+  
   # explicitly change 10 and 20 percent of community affiliations to add lines to graph
   comms <- unique(truemembership)
   lengths <- NULL
@@ -79,7 +87,7 @@ evalClust <- function(sym.matrix){
   perc20 <- round(.20*length(truemembership))
   tochange <- which(truemembership == comms[max])
   changed10 <- truemembership
-   for (p in 1:perc10)
+  for (p in 1:perc10)
     changed10[tochange[p]] <- comms[sample(which(lengths == min(lengths)[1]), 1)]
   rep10ari <- matrix(arandi(changed10, truemembership), 1, length(percent))
   rep10vi <- matrix(vi.dist(changed10, truemembership), 1, length(percent))
@@ -88,17 +96,17 @@ evalClust <- function(sym.matrix){
     changed20[tochange[p]] <- comms[sample(which(lengths == min(lengths)[1]), 1)]
   rep20ari <- matrix(arandi(changed20, truemembership), 1, length(percent))
   rep20vi <- matrix(vi.dist(changed20, truemembership), 1, length(percent))
-
+  
   plotARI <- plot(percentlab, colMeans(ARI.rando), col = "red", main = "Comparison of original result against perturbed graphs: ARI", xlab = "Proportion Perturbed", ylab = "Mean ARI")
   plotARI <-  points(percentlab, colMeans(ARI), col = "black") + lines(percentlab, rep10ari) + lines(percentlab, rep20ari)
-
+  
   plotVI <- plot(percentlab, colMeans(VI.rando), col = "red", main = "Comparison of original result against perturbed graphs: VI", xlab = "Proportion Perturbed", ylab = "Mean VI")
   plotVI <- plotVI + points(percentlab, colMeans(VI), col = "black") + lines(percentlab, rep10vi) + lines(percentlab, rep20vi)
-
+  
   res <- list(
     VI = VI,
     ARI = ARI,
-    modularity = modularity,
+    modularity = modularity.value,
     VI.rando = VI.rando,
     ARI.rando = ARI.rando,
     modularity.rando = modularity.rando,
--- R/rewireR.R
+++ R/rewireR.R
@@ -1,28 +1,46 @@
 ## this function follows from the work describe in "The weighted random graph model" by Garlaschelli,
 ## New Journal of Physics, 11, (2009), 073005.
 # only undirected, weighted graphs are considered here.
-# nperturb is the number of edges to perturb. 
-# matrix is the symmetric graph to rewire. 
-rewirematrix <- function(matrix, nperturb)
+# nperturb is the number of edges to perturb.
+# matrix is the symmetric graph to rewire.
+rewirematrix <- function(matrix, nperturb, type)
 {
-  W <- sum(colSums(matrix))/2 # sum of strengths; divide by two since undirected
-  # Optimal parameter choice - retain overall weight, not edge number
-  pstar <- (2*W)/(length(matrix[,1])*(length(matrix[,1])-1)+2*W)
-
-  dump <- matrix(1, length(matrix[,1]), length(matrix[,1]))
-  diag(dump) <- 0
-  eligable <- which(lower.tri(dump)!=0,arr.ind = T)#ensure that diagonal isn't considered, and all potential edges are
-  toalter <- sample(1:length(eligable[,1]), nperturb)
+  diag(matrix) <- 0
+  matrix[which(matrix < 0)] = 0 # set negative values to zero; not considered in most comm. det. approaches
+  eligable <-
+    which(lower.tri(matrix), arr.ind = T)#ensure that diagonal isn't considered, and all potential edges are
+  toalter <- sample(1:length(eligable[, 1]), nperturb)
   new.v <- matrix
-  for (l in 1:length(toalter)){
-    new.v[eligable[toalter[l],1],eligable[toalter[l],2]]<- 0
-    bernoulli <- 1
-    while (bernoulli == 1){
-      bernoulli <- rbinom(1,1,pstar)
-      new.v[eligable[toalter[l],1],eligable[toalter[l],2]]<- new.v[eligable[toalter[l],1],eligable[toalter[l],2]] + bernoulli
+  if (type == "count") {
+    W <-
+      sum(colSums(matrix)) / 2 # sum of strengths; divide by two since undirected
+    # Optimal parameter choice - retain overall weight, not edge number; Garlaschelli 2009
+    pstar <- (2 * W) / (length(matrix[, 1]) * (length(matrix[, 1]) - 1) +
+                          2 * W)
+    for (l in 1:length(toalter)) {
+      # new.v[eligable[toalter[l],1],eligable[toalter[l],2]]<- 0
+      bernoulli <- 1
+      toadd <- 0
+      while (bernoulli == 1) {
+        bernoulli <- rbinom(1, 1, pstar)
+        toadd <- bernoulli + toadd
+      }
+      new.v[eligable[toalter[l], 1], eligable[toalter[l], 2]] <-
+        toadd
+      }
+    elseif (type == "cov")
+    {
+      if (length(toalter)>1)
+      {
+        randomized <- sample(toalter)
+      } else {
+        randomized <- toalter
+      }
+      for (l in 1:length(randomized))
+        new.v[eligable[toalter[l],1],eligable[toalter[l],2]]<- new.v[[eligable[randomized[l],1],eligable[randomized[l],2]]] 
     }
-    # maintain symmetry:
-    new.v[eligable[toalter[l],2],eligable[toalter[l],1]]<- new.v[eligable[toalter[l],1],eligable[toalter[l],2]]
   }
+  new.v[upper.tri(new.v)] <-
+    t(new.v)[upper.tri(new.v)] # maintain symmetry
   return(as.data.frame(new.v))
 }
